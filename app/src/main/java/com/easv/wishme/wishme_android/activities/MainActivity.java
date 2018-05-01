d9bb9ce3d426fac4a257530b7f670e5a33d3ced0
Carlos <carl7755@easv365.dk>
2018-05-01 19:53:49 +0200

    wishes from firebase but not in realtime

--- app/src/main/java/com/easv/wishme/wishme_android/activities/MainActivity.java
+++ app/src/main/java/com/easv/wishme/wishme_android/activities/MainActivity.java
@@ -3,16 +3,23 @@ package com.easv.wishme.wishme_android.activities;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.easv.wishme.wishme_android.R;
 import com.easv.wishme.wishme_android.fragments.HomeFragment;
+import com.easv.wishme.wishme_android.utils.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started.");
+        initImageLoader();
         init();
     }
 
@@ -26,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void initImageLoader() {
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(MainActivity.this);
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
 
 }
 
--- app/src/main/java/com/easv/wishme/wishme_android/dal/DatabaseHelper.java
+++ app/src/main/java/com/easv/wishme/wishme_android/dal/DatabaseHelper.java
@@ -1,6 +1,52 @@
 package com.easv.wishme.wishme_android.dal;
 
+import com.easv.wishme.wishme_android.entities.Wish;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
 public class DatabaseHelper {
 
+ private static final String TAG = "DatabaseHelper";
 
+ private FirebaseFirestore mFirestore;
+
+ public DatabaseHelper() {
+ }
+
+// -----------------------------------------------------------------------------
+ // -----------------------------------------------------------------------------
+ //                  GET ALL WISHES
+
+ /**
+  * Retrieve all contacts from database
+  *
+  * @return
+  */
+ public ArrayList<Wish> getAllWishes() {
+  final ArrayList<Wish> list = new ArrayList<Wish>();
+
+
+  // THIS DOESN'T WORK.... WEIRD...(?)
+
+
+//  mFirestore = FirebaseFirestore.getInstance();
+//  mFirestore.collection("wish")
+//          .get()
+//          .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//           @Override
+//           public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//            if (task.isSuccessful()) {
+//             for (QueryDocumentSnapshot document : task.getResult()) {
+////              Log.d(TAG, document.getId() + " => " + document.getData());
+//              Wish wish = document.toObject(Wish.class);
+//              list.add(wish);
+//             }
+//            } else {
+//             Log.d(TAG, "Error getting documents: ", task.getException());
+//            }
+//           }
+//          });
+  return list;
+ }
 }
