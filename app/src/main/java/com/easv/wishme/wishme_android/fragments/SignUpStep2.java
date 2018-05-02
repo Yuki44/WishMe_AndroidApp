e1ff0d31be519308a92d6fe0ae93304c57cf7373
Carlos <carl7755@easv365.dk>
2018-05-02 14:24:12 +0200

    login UI, singup file changed name

--- app/src/main/java/com/easv/wishme/wishme_android/fragments/SignUpStep2.java
+++ app/src/main/java/com/easv/wishme/wishme_android/fragments/SignUpStep2.java
@@ -3,18 +3,15 @@ package com.easv.wishme.wishme_android.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.easv.wishme.wishme_android.R;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class CreateUserFragment2 extends Fragment {
+public class SignUpStep2 extends Fragment {
 
     private static final String TAG = "CreateUserFragment2";
     private CircleImageView profileImage;
@@ -23,7 +20,7 @@ public class CreateUserFragment2 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_create_user_2, container, false);
+        View view = inflater.inflate(R.layout.fragment_signup_step_2, container, false);
 
         profileImage = view.findViewById(R.id.profileImage);
 
